468676c1952cdb1905a227980fbc309f2236bfe8
davnpsh <git@davnpsh.dev>
2024-05-02 22:40:20 -0500

    Progress at home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import Navbar from "@/components/navbar-home";
+import "../styles/home-bg.css";
 
 export default function Home() {
-  return <>
-    <Navbar />
-  </>;
+  return (
+    <>
+      {/* Background */}
+      <div className="home-bg-light block dark:hidden"></div>
+      <div className="home-bg-dark hidden dark:block"></div>
+
+      <Navbar />
+    </>
+  );
 }
--- src/components/navbar-home.tsx
+++ src/components/navbar-home.tsx
@@ -1,23 +1,38 @@
 import { Button } from "@/components/ui/button";
-import { Github, LogIn, Pencil } from "lucide-react"
+import { Github, LogIn, Pencil } from "lucide-react";
 import Image from "next/image";
+import { ModeToggle } from "./ownui/theme-toggle";
+import { Badge } from "@/components/ui/badge";
+import Logo from "../../public/logo.png";
+import LogoDark from "../../public/logo-dark.png";
 
 export default function Navbar() {
   return (
-    <nav className="h-14 w-full px-8 border-b border-white border-opacity-5">
-      <div className="mx-auto max-w-screen-xl">
-        <div className="">
-          <Image src="/logo-inverted.png" width={40} height={40} alt="Logo" />
-          <span className="navbar__brand">Calcium</span>
+    <nav className="h-14 w-full px-8 border-b border-gray-200 dark:border-white dark:border-opacity-10">
+      <div className="mx-auto max-w-screen-xl flex items-center h-full justify-between">
+        <div className="flex items-center gap-2">
+          <div className="block dark:hidden">
+            <Image src={Logo} width={40} height={40} alt="Logo" />
+          </div>
+          <div className="hidden dark:block">
+            <Image src={LogoDark} width={40} height={40} alt="Logo" />
+          </div>
+          <span className="brand font-bold text-lg">Calcium</span>
+          <Badge>beta</Badge>
         </div>
-        <div className="">
+        <div className="flex items-center gap-2">
           <Button>
             <Pencil className="mr-2 h-4 w-4" /> Register
           </Button>
           <Button>
             <LogIn className="mr-2 h-4 w-4" /> Login
           </Button>
-          <Github />
+          <div className="px-3">
+            <a href="https://github.com/davnpsh/calcium-frontend">
+              <Github />
+            </a>
+          </div>
+          <ModeToggle />
         </div>
       </div>
     </nav>
