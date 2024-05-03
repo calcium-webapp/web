b3bdbd28364767799d9c0b3dddb3a0fca2ece89a
davnpsh <git@davnpsh.dev>
2024-05-03 01:44:52 -0500

    Fixed typo in credits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,10 +16,21 @@ export default function Home() {
       <div className="fixed bottom-0 w-[100vw]">
         <p className="text-center mb-2">
           Made with <Heart className="inline-block" /> by{" "}
-          <a className="font-bold" href="https://github.com/davnpsh">davnpsh</a>,{" "}
-          <a className="font-bold" href="https://github.com/Rcgil30">Rcgil30</a>,{" "}
-          <a className="font-bold" href="https://github.com/FiboDev">Fibodev</a> and{" "}
-          <a className="font-bold" href="https://github.com/K3nnyZY">K3nnyZY</a>
+          <a className="font-bold" href="https://github.com/davnpsh">
+            davnpsh
+          </a>
+          ,{" "}
+          <a className="font-bold" href="https://github.com/Rcgil30">
+            Rcgil30
+          </a>
+          ,{" "}
+          <a className="font-bold" href="https://github.com/FiboDev">
+            FiboDev
+          </a>{" "}
+          and{" "}
+          <a className="font-bold" href="https://github.com/K3nnyZY">
+            K3nnyZY
+          </a>
         </p>
       </div>
     </>
