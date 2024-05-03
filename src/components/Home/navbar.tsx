90bfc162f60f75a2160ffcfe8a7456aafc524188
davnpsh <git@davnpsh.dev>
2024-05-03 10:12:04 -0500

    Centered things correctly

--- src/components/Home/navbar.tsx
+++ src/components/Home/navbar.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="h-14 w-full px-8 border-b border-gray-200 dark:border-white dark:border-opacity-10">
+    <nav className="h-14 w-full px-8 border-b border-gray-200 dark:border-white dark:border-opacity-10 absolute">
       <div className="mx-auto max-w-screen-xl flex items-center h-full justify-between">
         <div className="flex items-center gap-2">
           <Link href="/">
--- src/components/Register/form.tsx
+++ src/components/Register/form.tsx
@@ -24,54 +24,56 @@ import { FaGoogle, FaGithub } from "react-icons/fa";
 
 export function RegisterForm() {
   return (
-    <Card className="w-[500px]">
-      <CardHeader>
-        <CardTitle>Create an account</CardTitle>
-        <CardDescription>
-          Select an option below to create your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="providers grid grid-cols-2 gap-6">
-              <Button variant="outline">
-                <FaGoogle className="mr-2 h-4 w-4" /> Google
-              </Button>
-              <Button variant="outline">
-                <FaGithub className="mr-2 h-4 w-4" /> Github
-              </Button>
-            </div>
-            {/* outline */}
-            <div className="relative">
-              <div className="absolute inset-0 flex items-center">
-                <span className="w-full border-t"></span>
+    <div className="grid h-screen place-items-center">
+      <Card className="w-[500px]">
+        <CardHeader>
+          <CardTitle>Create an account</CardTitle>
+          <CardDescription>
+            Select an option below to create your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="providers grid grid-cols-2 gap-6">
+                <Button variant="outline">
+                  <FaGoogle className="mr-2 h-4 w-4" /> Google
+                </Button>
+                <Button variant="outline">
+                  <FaGithub className="mr-2 h-4 w-4" /> Github
+                </Button>
               </div>
-              <div className="relative flex justify-center text-xs uppercase">
-                <span className="bg-background px-2 text-muted-foreground">
-                  Or continue with
-                </span>
+              {/* outline */}
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <span className="w-full border-t"></span>
+                </div>
+                <div className="relative flex justify-center text-xs uppercase">
+                  <span className="bg-background px-2 text-muted-foreground">
+                    Or continue with
+                  </span>
+                </div>
+              </div>
+              {/* end outline */}
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Username</Label>
+                <Input id="name" placeholder="Name for your account" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" placeholder="user@calcium.dev" type="email" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" />
               </div>
             </div>
-            {/* end outline */}
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Username</Label>
-              <Input id="name" placeholder="Name for your account" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" placeholder="user@calcium.dev" type="email" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" />
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="">
-        <Button className="w-full">Register</Button>
-      </CardFooter>
-    </Card>
+          </form>
+        </CardContent>
+        <CardFooter className="">
+          <Button className="w-full">Register</Button>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
