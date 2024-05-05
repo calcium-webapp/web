062567e6c059333bd4b60d925c750f147ac9b3be
davnpsh <git@davnpsh.dev>
2024-05-05 16:36:39 -0500

    Added animations for each board

--- src/app/my-boards/page.tsx
+++ src/app/my-boards/page.tsx
@@ -15,7 +15,9 @@ export default function Boards() {
       <Navbar />
       <main className="flex-1 px-8 overflow-hidden">
         <div className="main_content pt-5 mx-auto max-w-screen-xl flex flex-col h-full">
-          <TypographyH3 className="pb-2 shadow-[#fff_-5px_10px_11px_5px] dark:shadow-[#09090b_-5px_10px_11px_5px] z-10">My boards</TypographyH3>
+          <TypographyH3 className="pb-2 shadow-[#fff_-5px_10px_11px_5px] dark:shadow-[#09090b_-5px_10px_11px_5px] z-10">
+            My boards
+          </TypographyH3>
           <ScrollArea>
             <div className="boards w-full py-5 pr-3 grid grid-cols-3 gap-2">
               <Board />
--- src/components/MyBoards/board.tsx
+++ src/components/MyBoards/board.tsx
@@ -1,20 +1,45 @@
+"use client";
+
 import "@/styles/board.css";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { BoardOptions } from "./board-options";
+import { motion } from "framer-motion";
+import { useAnimate } from "framer-motion";
 
-const backgrounds: string[] = ['a', 'b', 'c', 'd', 'e', 'f'];
+const backgrounds: string[] = ["a", "b", "c", "d", "e", "f"];
 
 export function Board() {
   var randomIndex: number = Math.floor(Math.random() * backgrounds.length);
   var randomBg: string = backgrounds[randomIndex];
+  const [scope, animate] = useAnimate();
+
+  function runAnimation(scaleValue: number) {
+    animate(
+      scope.current,
+      {
+        scale: scaleValue,
+      },
+      { type: "spring", stiffness: 400, damping: 10 }
+    );
+  }
 
   return (
-    <div className="board rounded-lg ">
-      <AspectRatio ratio={16 / 9} className="">
-        <div className={`preview ${randomBg} h-3/4 w-full rounded-t-lg`}></div>
+    <div className="board rounded-lg hover:z-20" ref={scope}>
+      <AspectRatio ratio={16 / 9}>
+        <motion.div
+          className={`preview ${randomBg} h-3/4 w-full rounded-t-lg cursor-pointer`}
+          onHoverStart={() => {
+            runAnimation(1.05);
+          }}
+          onHoverEnd={() => {
+            runAnimation(1);
+          }}
+        ></motion.div>
         <div className="info h-1/4 w-full rounded-b-lg border-2 bg-background flex justify-between items-center px-3">
           <div className="board-name overflow-hidden">
-            <span className="font-bold text-gray-600 dark:text-gray-100">beautiful-tiger</span>
+            <span className="font-bold text-gray-600 dark:text-gray-100">
+              beautiful-tiger
+            </span>
           </div>
           <BoardOptions />
         </div>
