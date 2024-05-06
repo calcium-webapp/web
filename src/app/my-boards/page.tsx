9f0552c46de9f7bfee1995a17d7c9d6571685da3
davnpsh <git@davnpsh.dev>
2024-05-06 15:08:45 -0500

    Fix on z-index

--- src/app/my-boards/page.tsx
+++ src/app/my-boards/page.tsx
@@ -16,7 +16,7 @@ export default function Boards() {
       <Navbar />
       <main className="flex-1 px-8 overflow-y-hidden">
         <div className="main_content pt-5 mx-auto max-w-screen-xl flex flex-col h-full">
-          <TypographyH3 className="pb-2 shadow-[#fff_-5px_10px_11px_5px] dark:shadow-[#09090b_-5px_10px_11px_5px] z-10">
+          <TypographyH3 className="pb-2 shadow-[#fff_-5px_10px_11px_5px] dark:shadow-[#09090b_-5px_10px_11px_5px] z-30">
             My boards
           </TypographyH3>
           <ScrollArea>
