d50772a10d65e9fdbb47a88471389f426a3842e9
davnpsh <git@davnpsh.dev>
2024-05-06 14:53:33 -0500

    Improved animations functios by using React Hooks

--- src/components/MyBoards/board.tsx
+++ src/components/MyBoards/board.tsx
@@ -5,6 +5,7 @@ import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { BoardOptions } from "./board-options";
 import { motion } from "framer-motion";
 import { useAnimate } from "framer-motion";
+import { useState, useMemo } from "react";
 
 interface BoardProps {
   name: string;
@@ -13,30 +14,27 @@ interface BoardProps {
 const backgrounds: string[] = ["a", "b", "c", "d", "e", "f"];
 
 export function Board({ name }: BoardProps) {
-  var randomIndex: number = Math.floor(Math.random() * backgrounds.length);
+  var randomIndex: number = useMemo(
+    () => Math.floor(Math.random() * backgrounds.length),
+    []
+  );
   var randomBg: string = backgrounds[randomIndex];
-  const [scope, animate] = useAnimate();
-
-  function runAnimation(scaleValue: number) {
-    animate(
-      scope.current,
-      {
-        scale: scaleValue,
-      },
-      { type: "spring", stiffness: 400, damping: 10 }
-    );
-  }
+  const [scale, setScale] = useState<number>(1);
 
   return (
-    <div className="board rounded-lg hover:z-20 hover:shadow-2xl" ref={scope}>
+    <motion.div
+      className="board rounded-lg hover:z-20 hover:shadow-2xl"
+      whileHover={{ scale: scale }}
+      transition={{ type: "spring", stiffness: 400, damping: 10 }}
+    >
       <AspectRatio ratio={16 / 9}>
         <motion.div
           className={`preview ${randomBg} h-3/4 w-full rounded-t-lg cursor-pointer`}
           onHoverStart={() => {
-            runAnimation(1.05);
+            setScale(1.05);
           }}
           onHoverEnd={() => {
-            runAnimation(1);
+            setScale(1);
           }}
         ></motion.div>
         <div className="info h-1/4 w-full rounded-b-lg border-2 bg-background flex justify-between items-center px-3">
@@ -48,6 +46,6 @@ export function Board({ name }: BoardProps) {
           <BoardOptions />
         </div>
       </AspectRatio>
-    </div>
+    </motion.div>
   );
 }
