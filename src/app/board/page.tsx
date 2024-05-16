c572bddb48564600cde79d703c2605cb9dec3386
davnpsh <git@davnpsh.dev>
2024-05-15 23:32:50 -0500

    Added layout and code editor liveblock dependencies

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,13 +1,39 @@
 "use client";
 
-import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
 import { Navbar } from "@/components/Board/navbar";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 export default function Board() {
   return (
     <div className="flex flex-col h-screen">
       <Navbar />
+      <main className="flex-1 p-6 overflow-y-hidden">
+        <ResizablePanelGroup
+          direction="horizontal"
+          className="rounded-lg border"
+        >
+          <ResizablePanel defaultSize={50} minSize={40} maxSize={60}>
+            {/* Whiteboard */}
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={50}>
+            <ResizablePanelGroup direction="vertical">
+              <ResizablePanel defaultSize={50} minSize={40} maxSize={60}>
+                {/* Code editor */}
+              </ResizablePanel>
+              <ResizableHandle withHandle />
+              <ResizablePanel defaultSize={50}>
+                {/* Terminal */}
+              </ResizablePanel>
+            </ResizablePanelGroup>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </main>
     </div>
   );
 }
