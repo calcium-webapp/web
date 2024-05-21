483807588868cce69829727ec33297abca123d09
davnpsh <git@davnpsh.dev>
2024-05-20 20:58:23 -0500

    Added mock data

--- src/app/api/liveblocks-auth/route.ts
+++ src/app/api/liveblocks-auth/route.ts
@@ -10,6 +10,48 @@ import { getServerSession } from "next-auth";
 
  */
 
+const MOCK_INFO = [
+  {
+    color: "#D583F0",
+    picture: "https://liveblocks.io/avatars/avatar-1.png",
+  },
+
+  {
+    color: "#F08385",
+    picture: "https://liveblocks.io/avatars/avatar-2.png",
+  },
+
+  {
+    color: "#F0D885",
+    picture: "https://liveblocks.io/avatars/avatar-3.png",
+  },
+
+  {
+    color: "#85EED6",
+    picture: "https://liveblocks.io/avatars/avatar-4.png",
+  },
+
+  {
+    color: "#85BBF0",
+    picture: "https://liveblocks.io/avatars/avatar-5.png",
+  },
+
+  {
+    color: "#8594F0",
+    picture: "https://liveblocks.io/avatars/avatar-6.png",
+  },
+
+  {
+    color: "#85DBF0",
+    picture: "https://liveblocks.io/avatars/avatar-7.png",
+  },
+
+  {
+    color: "#87EE85",
+    picture: "https://liveblocks.io/avatars/avatar-8.png",
+  },
+];
+
 const liveblocks = new Liveblocks({
   secret: process.env.LIVEBLOCKS_SECRET_KEY!,
 });
@@ -17,15 +59,16 @@ const liveblocks = new Liveblocks({
 export async function POST(request: NextRequest) {
   // Get the current user's unique id from your database
   const nextauth_session = await getServerSession();
-  console.log(nextauth_session);
+
+  // Mock data in case of missing data
+  const mockIndex = Math.floor(Math.random() * 10) % MOCK_INFO.length;
 
   // Create a session for the current user
-
   const session = liveblocks.prepareSession(`mock-user`, {
     userInfo: {
       name: nextauth_session?.user.name,
-      color: "#D583F0",
-      picture: nextauth_session?.user.image,
+      color: MOCK_INFO[mockIndex].color,
+      picture: nextauth_session?.user.image ? nextauth_session.user.image : MOCK_INFO[mockIndex].picture,
     },
   });
 
